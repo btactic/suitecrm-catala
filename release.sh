518fed3a3bd869a295d64bd083b745467260fd90
Btactic Translate 2 <btactic.translate.2@btactic.com>
2016-02-10 11:07:43 +0100

    Added release script * This script creates an installable version of SuiteCRM Catalan translation

---
+++ release.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+zip -r suitecrm-catala.zip custom images include install modules manifest.php
+
+echo "INFO: An installable version of translation is available on suitecrm-catala.zip"
+echo "INFO: Now you can install the catalan translation on a SuiteCRM installation via the Module Loader"
+
+exit 0
