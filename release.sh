099a6c4a4a9b45a4b816c7a017b8d03cd0440096
Adrian Gibanel bTactic <adrian.gibanel@btactic.com>
2020-02-27 12:45:34 +0100

    New releases directory

--- release.sh
+++ release.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-zip -r suitecrm-catala.zip custom images include install modules manifest.php
+zip -r releases/suitecrm-catala.zip custom images include install modules manifest.php
 
-echo "INFO: An installable version of translation is available on suitecrm-catala.zip"
+echo "INFO: An installable version of translation is available on releases/suitecrm-catala.zip"
 echo "INFO: Now you can install the catalan translation on a SuiteCRM installation via the Module Loader"
 
 exit 0
